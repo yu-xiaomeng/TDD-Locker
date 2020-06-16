6a7a6aff62570410a3b93767b26759bb10fe633e
yu-xiaomeng <414017727@qq.com>
2020-06-16 22:59:01 +0800

    [mengling&xiongli] checkout will failed when use a reused ticket

--- src/test/java/com/thoughtworks/locker/LockerTest.java
+++ src/test/java/com/thoughtworks/locker/LockerTest.java
@@ -50,7 +50,7 @@ public class LockerTest {
     }
 
     @Test(expected = RuntimeException.class)
-    public void should_check_out_failed_when_check_out_given_ticket_is_invalid_or_duplicate() {
+    public void should_check_out_failed_when_check_out_given_ticket_is_invalid() {
         // Given
         Locker locker = new Locker(10);
         Ticket ticket = new Ticket();
@@ -60,6 +60,22 @@ public class LockerTest {
             locker.checkOut(ticket);
         } catch (Exception e) {
             // Then
+            Assert.assertEquals("非法票据", e.getMessage());
+            throw e;
+        }
+
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void should_check_out_failed_when_check_out_given_ticket_is_reused() {
+        Locker locker = new Locker(10);
+        Bag checkInBag = new Bag();
+        Ticket ticket = locker.checkIn(checkInBag);
+        locker.checkOut(ticket);
+
+        try {
+            locker.checkOut(ticket);
+        } catch (Exception e) {
             Assert.assertEquals("非法票据", e.getMessage());
             throw e;
         }
