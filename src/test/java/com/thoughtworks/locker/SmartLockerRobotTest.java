bae1b4c1ac88f6d74f3bc4e4647ddd2802be3ed8
zhengxiongli <xlzheng@thoughtworks.com>
2020-06-20 20:24:02 +0800

    [zhengxiongli] should_check_in_success_in_1st_locker_and_get_ticket_when_smart_robot_check_in_given_locker1_capacity_is_2_and_locker2_capacity_is_2

--- src/test/java/com/thoughtworks/locker/SmartLockerRobotTest.java
+++ src/test/java/com/thoughtworks/locker/SmartLockerRobotTest.java
@@ -32,4 +32,17 @@ public class SmartLockerRobotTest {
         Assert.assertNotNull(ticket);
         Assert.assertEquals(bag, locker2.checkOut(ticket));
     }
+
+    @Test
+    public void should_check_in_success_in_1st_locker_and_get_ticket_when_smart_robot_check_in_given_locker1_capacity_is_2_and_locker2_capacity_is_2() {
+        Locker locker1 = new Locker(2);
+        Locker locker2 = new Locker(2);
+        SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(locker1, locker2));
+
+        Bag bag = new Bag();
+        Ticket ticket = smartLockerRobot.checkIn(bag);
+
+        Assert.assertNotNull(ticket);
+        Assert.assertEquals(bag, locker1.checkOut(ticket));
+    }
 }
