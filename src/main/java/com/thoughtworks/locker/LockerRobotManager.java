70bc738eefc45a98e2238730b5d068b6c78236fa
zhengxiongli <xlzheng@thoughtworks.com>
2020-06-23 23:27:53 +0800

    [zhengxiongli] refactor

--- src/main/java/com/thoughtworks/locker/LockerRobotManager.java
+++ src/main/java/com/thoughtworks/locker/LockerRobotManager.java
@@ -28,14 +28,13 @@ public class LockerRobotManager {
     }
 
     public LockerRobotManagerTicket checkIn(Bag bag) {
-        Locker locker = this.allLockers.stream().filter(l -> !l.isFull()).findFirst().orElseThrow(LockerFullException::new);
-//        Locker locker = getRobotLocker();
-//        if (locker == null && !isNullOrEmpty(lockers)) {
-//            locker = this.lockers.stream().filter(l -> !l.isFull()).findFirst().orElseThrow(LockerFullException::new);
-//        }
-//        if (locker == null) {
-//            throw new LockerFullException();
-//        }
+        Locker locker = getRobotLocker();
+        if (locker == null && !isNullOrEmpty(lockers)) {
+            locker = this.lockers.stream().filter(l -> !l.isFull()).findFirst().orElseThrow(LockerFullException::new);
+        }
+        if (locker == null) {
+            throw new LockerFullException();
+        }
         return new LockerRobotManagerTicket(locker.checkIn(bag));
     }
 
