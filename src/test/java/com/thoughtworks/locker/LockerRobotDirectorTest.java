d54dca21af1f724c0dd2646d0e0852aee77c9ca3
yu-xiaomeng <414017727@qq.com>
2020-07-01 20:08:49 +0800

    [yumengling] should_successful_when_director_print_report_given_director_manage_one_manager_and_manager_manage_one_robot_and_one_locker

--- src/test/java/com/thoughtworks/locker/LockerRobotDirectorTest.java
+++ src/test/java/com/thoughtworks/locker/LockerRobotDirectorTest.java
@@ -31,4 +31,21 @@ public class LockerRobotDirectorTest {
 
         Assert.assertEquals("M 3 5\n\tR 3 5\n\t\tL 3 5", report);
     }
+
+    @Test
+    public void should_successful_when_director_print_report_given_director_manage_one_manager_and_manager_manage_one_robot_and_one_locker() {
+        AbstractLockerRobot robot = new PrimaryLockerRobot(Arrays.asList(new Locker(5)));
+        Locker locker = new Locker(8);
+        LockerRobotManager manager = new LockerRobotManager(Arrays.asList(robot), Arrays.asList(locker));
+        manager.checkIn(new Bag());
+        manager.checkIn(new Bag());
+        locker.checkIn(new Bag());
+        locker.checkIn(new Bag());
+        locker.checkIn(new Bag());
+        LockerRobotDirector director = new LockerRobotDirector(Arrays.asList(manager));
+
+        String report = director.print();
+
+        Assert.assertEquals("M 8 13\n\tL 5 8\n\tR 3 5\n\t\tL 3 5", report);
+    }
 }
